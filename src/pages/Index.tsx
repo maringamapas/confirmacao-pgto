9dc1609738bd51555cb6c135ec0a9f284cfc836e
CHL <maringamapas@gmail.com>
2026-05-25 16:27:44 -0300

    Initialize v0/sueketziahchristal2-8023-5a7aa857

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import { Component } from "@/components/ui/gradient-bar-hero-section";
+
+const Index = () => {
+  return (
+    <div className="w-full h-screen bg-black">
+      <Component />
+    </div>
+  );
+};
+
+export default Index;
